8b65694aa732a06700d5e050aea4fbbca2a282c2
skiftyy <77881859+skiftyy@users.noreply.github.com>
2021-03-26 13:43:55 +1000

    Improved Speed, Sound and Executable
    Improved speed and sound for better effect. A new icon that looks like a prototype from google to encourage the user to click the file.

--- SoundSpammer.py
+++ SoundSpammer.py
@@ -1,11 +1,12 @@
 import pyautogui, time, winsound
 
 f = 100
-l = 100
+
+for i in range(50):
+    pyautogui.press("volumeup")
 
 while 0==0:
     pyautogui.press("volumeup")
-    winsound.Beep(f, l)
-    l += 100
-    if f < 20000:
+    winsound.Beep(f, 500)
+    if f < 8000:
         f += 100
