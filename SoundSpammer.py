1939da315cfd25f40d57c6f2261a9338ec7fbbf1
skiftyy <77881859+skiftyy@users.noreply.github.com>
2021-05-13 11:34:51 +1000

    Update SoundSpammer.py
    Faster noise

--- SoundSpammer.py
+++ SoundSpammer.py
@@ -6,7 +6,7 @@ for i in range(50):
     pyautogui.press("volumeup")
 
 while 0==0:
+    winsound.Beep(f, 200)
     pyautogui.press("volumeup")
-    winsound.Beep(f, 500)
     if f < 8000:
         f += 100
